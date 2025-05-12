789d69bb800dc7ca873b1502aca9ff3451d360f9
mo3tazJ <en.mo3taz@gmail.com>
2025-05-12 22:46:25 +0300

    R Code File Created

---
+++ code.R
@@ -0,0 +1,10 @@
+weightmedian <- function(directory, day) {
+  files_list <- list.files(directory, full.names = TRUE)
+  file_list_length <- length(files_list)
+  dat <- data.frame()
+  for(i in 1:file_list_length) {
+    dat <- rbind(dat, read.csv(files_list[i]))
+  }
+  day_weight <- dat[dat$Day == day, "Weight"]
+  weightmedian <- median(day_weight, na.rm = TRUE)
+}
